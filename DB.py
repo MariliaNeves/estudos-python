60a404db2c1e866bbaf145b3d9b3fbe2be8fb025
marilia.neves <marilia.neves@dextra-sw.com>
2021-09-14 14:14:18 -0400

    correção de codigo repetido

--- DB.py
+++ DB.py
@@ -11,52 +11,44 @@ def getConnection():
 def getCollection():
     connection = getConnection()
     return connection[DB_NAME][COLLECTION_NAME]
+collection = getCollection()
 
 def closeConnection(funcao):
     def wrapper(*args, **kwargs):
-        connection = getConnection()
         var = funcao(*args, **kwargs)
-        connection.close()
+        getConnection().close()
         return var
     return wrapper
 
 @closeConnection
 def insertComic(comic):
-    collection = getCollection()
     collection.insert_one(comic)
 
 @closeConnection
 def getComic(value):
-    collection = getCollection()
     return collection.find(value)
 
 @closeConnection
 def insertAllComics(comics):
-    collection = getCollection()
     collection.insert_many(comics)
 
 @closeConnection
 def getAllComics():
-    collection = getCollection()
     return collection.find()
 
 @closeConnection
 def createComic(value):
-    collection = getCollection()
     collection.insert_one(value)
 
 @closeConnection
 def updateComic(id, value):
-    collection = getCollection()
     collection.update({'id': int(id)}, value)
 
 @closeConnection
 def deleteComic(value):
-    collection = getCollection()
     collection.delete_one(value)
 
 @closeConnection
 def deleteAllComics():
-    collection = getCollection()
     collection.delete_many({})
 
